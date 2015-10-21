54ab32ce0b0b57b420cfb833c775b64b704db393
Rick G <lacunainc04@gmail.com>
2015-10-20 22:51:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// This is where the javascript goes
+var arrayTips = [
+{"tip":"Design every page as a landing page."},
+{"tip":"Make appealing form buttons"},
+{"tip":"Learn how to use google"},
+ {"tip":"Dry = Don't Repeat Yourself"},
+  {"tip":"When starting a new project, design it in shades of gray and add color later"},
+  {"tip":"Push to github often"},
+  {"tip":"Work on your online presence"}
+  
+];
+
+//Random number from the length of the array AND display of coding tip
+function randomTip() {
+var result = Math.floor((Math.random()*arrayTips.length));
+document.getElementById("random-tip").innerHTML = arrayTips[result].tip;
+
+ var rTipHighlight = document.querySelector("#random-tip");
+rTipHighlight.style.display = "inline";
+}
