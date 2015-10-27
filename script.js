766d82f55e6b67c46bfb7ff0c6b5686e646f89b5
Rick G <lacunainc04@gmail.com>
2015-10-27 01:52:32 -0700

    Add initalized code from mockup and add no index follow

--- script.js
+++ script.js
@@ -3,17 +3,18 @@ var arrayTips = [
 {"tip":"Make appealing form buttons"},
 {"tip":"Learn how to use google"},
  {"tip":"Dry = Don't Repeat Yourself"},
-  {"tip":"When starting a new project, design it in shades of gray and add color later"},
-  {"tip":"Push to github often"},
-  {"tip":"Work on your online presence"}
+  {"tip":"When starting a new project, design it in shades of gray and add color later"}
   
 ];
 
 //Random number from the length of the array AND display of coding tip
 function randomTip() {
-var result = Math.floor((Math.random()*arrayTips.length));
-document.getElementById("random-tip").innerHTML = arrayTips[result].tip;
+var result = Math.floor((Math.random()*arrayTips.length) );
+var tweetText = document.getElementById("random-tip").innerHTML = arrayTips[result].tip;
 
- var rTipHighlight = document.querySelector("#random-tip");
+var rTipHighlight = document.querySelector("#random-tip");
 rTipHighlight.style.display = "inline";
+//Adding Twitter
+document.getElementById("tweet-div").innerHTML='<a  href="https://twitter.com/intent/tweet?text=' + tweetText.replace(/\s+/g, "%20") + '" class="btn btn-lg twitter-share-button" id="tweetTip" data-size="large">Tweet</a>';
+
 }
